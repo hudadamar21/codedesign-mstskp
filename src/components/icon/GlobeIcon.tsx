2562ad4e562cfa37e6bc85d73f5446b431fb46e6
hudadamar21 <hudadamar21@gmail.com>
2023-05-24 21:39:51 +0700

    Create GlobeIcon.tsx

---
+++ src/components/icon/GlobeIcon.tsx
@@ -0,0 +1,36 @@
+function GlobeIcon() {
+  return (
+    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <g clip-path="url(#clip0_4_366)">
+        <path
+          d="M10 18.3333C14.6024 18.3333 18.3334 14.6023 18.3334 9.99996C18.3334 5.39759 14.6024 1.66663 10 1.66663C5.39765 1.66663 1.66669 5.39759 1.66669 9.99996C1.66669 14.6023 5.39765 18.3333 10 18.3333Z"
+          stroke="#FC881D"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M1.66669 10H18.3334"
+          stroke="#FC881D"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M10 1.66663C12.0844 3.94859 13.269 6.90999 13.3334 9.99996C13.269 13.0899 12.0844 16.0513 10 18.3333C7.91562 16.0513 6.73106 13.0899 6.66669 9.99996C6.73106 6.90999 7.91562 3.94859 10 1.66663V1.66663Z"
+          stroke="#FC881D"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_4_366">
+          <rect width="20" height="20" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
+
+export default GlobeIcon;
