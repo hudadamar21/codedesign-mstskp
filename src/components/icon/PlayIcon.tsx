99fce18a03396ad96bed180c67d59aa32912b613
hudadamar21 <hudadamar21@gmail.com>
2023-05-24 21:39:53 +0700

    Create PlayIcon.tsx

---
+++ src/components/icon/PlayIcon.tsx
@@ -0,0 +1,22 @@
+function PlayIcon() {
+  return (
+    <svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M9.99999 18.8334C14.6024 18.8334 18.3333 15.1024 18.3333 10.5C18.3333 5.89765 14.6024 2.16669 9.99999 2.16669C5.39762 2.16669 1.66666 5.89765 1.66666 10.5C1.66666 15.1024 5.39762 18.8334 9.99999 18.8334Z"
+        stroke="white"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M8.33331 7.16669L13.3333 10.5L8.33331 13.8334V7.16669Z"
+        stroke="white"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
+
+export default PlayIcon;
