15f1282508dc391bc1c9a084e612ac990ee4c49b
hudadamar21 <hudadamar21@gmail.com>
2023-05-24 21:42:10 +0700

    create footer menu data

---
+++ src/data/footer-menu.tsx
@@ -0,0 +1,39 @@
+export const footerMenus = [
+  {
+    name: "Alternative",
+    list: [
+      { name: "Buffer", url: "#" },
+      { name: "Sendible", url: "#" },
+      { name: "Later", url: "#" },
+      { name: "Social Pilot", url: "#" },
+    ],
+  },
+  {
+    name: "Sitemap",
+    list: [
+      { name: "Top", url: "#" },
+      { name: "Features", url: "#" },
+      { name: "Reason", url: "#" },
+      { name: "Alternatives", url: "#" },
+      { name: "Login", url: "#" },
+    ],
+  },
+  {
+    name: "Company",
+    list: [
+      { name: "About Us", url: "#" },
+      { name: "Contact", url: "#" },
+      { name: "Privacy Policy", url: "#" },
+      { name: "Terms of Services", url: "#" },
+      { name: "Help", url: "#" },
+    ],
+  },
+  {
+    name: "Get In Touch",
+    list: [
+      { name: "Twitter", url: "#" },
+      { name: "Facebook", url: "#" },
+      { name: "Dribble", url: "#" },
+    ],
+  },
+];
