ddec29a31ce93ffcade4b459e6921bd41ee0540f
hudadamar21 <hudadamar21@gmail.com>
2023-05-24 21:39:56 +0700

    Create ServerIcon.tsx

---
+++ src/components/icon/ServerIcon.tsx
@@ -0,0 +1,43 @@
+function ServerIcon() {
+  return (
+    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <g clip-path="url(#clip0_4_357)">
+        <path
+          d="M16.6667 1.66663H3.33335C2.41288 1.66663 1.66669 2.41282 1.66669 3.33329V6.66663C1.66669 7.5871 2.41288 8.33329 3.33335 8.33329H16.6667C17.5872 8.33329 18.3334 7.5871 18.3334 6.66663V3.33329C18.3334 2.41282 17.5872 1.66663 16.6667 1.66663Z"
+          stroke="#FC881D"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M16.6667 11.6666H3.33335C2.41288 11.6666 1.66669 12.4128 1.66669 13.3333V16.6666C1.66669 17.5871 2.41288 18.3333 3.33335 18.3333H16.6667C17.5872 18.3333 18.3334 17.5871 18.3334 16.6666V13.3333C18.3334 12.4128 17.5872 11.6666 16.6667 11.6666Z"
+          stroke="#FC881D"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M5 5H5.00833"
+          stroke="#FC881D"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M5 15H5.00833"
+          stroke="#FC881D"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_4_357">
+          <rect width="20" height="20" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
+
+export default ServerIcon;
