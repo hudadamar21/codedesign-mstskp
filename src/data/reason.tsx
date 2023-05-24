96d052c198fd86884ee668b0fe7b7113b51a73ae
hudadamar21 <hudadamar21@gmail.com>
2023-05-24 21:41:23 +0700

    create reasons data

---
+++ src/data/reason.tsx
@@ -0,0 +1,20 @@
+export const reasons = [
+  {
+    image: "/images/reason1.png",
+    name: "Kamu Tidak Punya Kekasih",
+    description:
+      "Sudah pasti orang yang tidak punya kekasih akan memiliki waktu luang yang banyak, jadi ya lo iseng aja gitu datang ke website ini.",
+  },
+  {
+    image: "/images/reason2.png",
+    name: "Lo Wibu Autid",
+    description:
+      "Iyh bener, biasanya wibu kalo nggak halu waifu suka iseng nggak jelas, termasuk mengunjungi website ini tanpa alasan yang jelas hehehe.",
+  },
+  {
+    image: "/images/reason3.png",
+    name: "Kok Gambar Ayam?",
+    description:
+      "Lah ngatur. Nggak tau lagi gwej alasan yang ketiga, biar bagus aja desainnya. Kalo cuman dua jadinya nggak bagus hehe.",
+  },
+];
