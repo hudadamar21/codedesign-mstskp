a409d3b09e2487b464aba847f57ad2db5d8a18ab
hudadamar21 <hudadamar21@gmail.com>
2023-05-24 21:42:53 +0700

    add all section to page

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+import AlternativeSection from "./components/sections/AlternativeSection";
+import FeaturesSection from "./components/sections/FeaturesSection";
+import ReasonSection from "./components/sections/ReasonSection";
 import TopSection from "./components/sections/TopSection";
 import MainLayout from "./layouts/MainLayout";
 
@@ -5,6 +8,9 @@ function App() {
   return (
     <MainLayout>
       <TopSection />
+      <FeaturesSection />
+      <ReasonSection />
+      <AlternativeSection />
     </MainLayout>
   );
 }
