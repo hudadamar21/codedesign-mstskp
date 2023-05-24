94540aa49a9ebc06943cc710de94fdf2f1c43a08
hudadamar21 <hudadamar21@gmail.com>
2023-05-24 21:56:54 +0700

    responsive mobile footer section

--- src/layouts/partial/MainFooter.tsx
+++ src/layouts/partial/MainFooter.tsx
@@ -2,8 +2,8 @@ import { footerMenus } from "../../data/footer-menu";
 
 function MainFooter() {
   return (
-    <footer className="bg-white pb-32">
-      <div className="main-container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-20">
+    <footer className="bg-white pb-32 p-10">
+      <div className="main-container grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-10 md:gap-20">
         {footerMenus.map((menu, index) => (
           <div key={index}>
             <h2 className="text-sm font-bold uppercase mb-4">{menu.name}</h2>
