2542da90da6b3264b74d711e066a342b2bfdf7f8
hudadamar21 <hudadamar21@gmail.com>
2023-05-24 21:41:44 +0700

    create alternative data

---
+++ src/data/alternatives.tsx
@@ -0,0 +1,14 @@
+export const alternatives = [
+  {
+    thumbnail: '/images/alter1.png',
+    name: 'buffer.com',
+    description: 'Buffer.com – Buffer is the most intuitive, affordable, and authentic way to reach more people on social media.',
+    url: '#'
+  },
+  {
+    thumbnail: '/images/alter2.png',
+    name: 'sendible.com',
+    description: 'Sendible.com – Grow your audience, attract new customers, and reach your social media goals with an all-in-one platform.',
+    url: '#'
+  },
+]
