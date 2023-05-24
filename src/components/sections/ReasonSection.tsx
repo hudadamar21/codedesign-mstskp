fee0685928b360cb672442f7b7368e713a77091d
hudadamar21 <hudadamar21@gmail.com>
2023-05-24 21:56:36 +0700

    responsive mobile reason section

--- src/components/sections/ReasonSection.tsx
+++ src/components/sections/ReasonSection.tsx
@@ -2,8 +2,8 @@ import { reasons } from "../../data/reason";
 
 function ReasonSection() {
   return (
-    <section className="relative main-container flex flex-col justify-center items-center mt-64">
-      <div className="space-y-6 text-center w-1/2 mb-20">
+    <section className="relative main-container flex flex-col justify-center items-center mt-32 md:mt-64 px-5">
+      <div className="space-y-6 text-center w-full md:w-1/2 mb-20">
         <div className="text-primary font-semibold tracking-widest">TERUS GIMANA?</div>
         <h2 className="font-bold text-4xl">Terus Ngapain Dong Kalo Nggak Ada Insight Apapun?</h2>
         <p className="opacity-60">
@@ -13,7 +13,7 @@ function ReasonSection() {
         </p>
       </div>
 
-      <div className="grid grid-cols-3 gap-10">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
         {reasons.map((reason, i) => (
           <div className="space-y-3" key={i}>
             <div className="aspect-video flex items-center justify-center w-full rounded-xl bg-dark/5">
