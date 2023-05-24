3cf0f182228fc9b9cda528e6f208e2da8a9365ff
hudadamar21 <hudadamar21@gmail.com>
2023-05-24 21:39:59 +0700

    Create ZapIcon.tsx

---
+++ src/components/icon/ZapIcon.tsx
@@ -0,0 +1,15 @@
+function ZapIcon() {
+  return (
+    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M10.8333 1.66663L2.5 11.6666H10L9.16667 18.3333L17.5 8.33329H10L10.8333 1.66663Z"
+        stroke="#FC881D"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
+
+export default ZapIcon;
