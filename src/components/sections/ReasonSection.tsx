1bcfac13e5031177a35956b8758fb29eb1837f91
hudadamar21 <hudadamar21@gmail.com>
2023-05-24 21:42:38 +0700

    Create ReasonSection.tsx

---
+++ src/components/sections/ReasonSection.tsx
@@ -0,0 +1,31 @@
+import { reasons } from "../../data/reason";
+
+function ReasonSection() {
+  return (
+    <section className="relative main-container flex flex-col justify-center items-center mt-64">
+      <div className="space-y-6 text-center w-1/2 mb-20">
+        <div className="text-primary font-semibold tracking-widest">TERUS GIMANA?</div>
+        <h2 className="font-bold text-4xl">Terus Ngapain Dong Kalo Nggak Ada Insight Apapun?</h2>
+        <p className="opacity-60">
+          Ya nggak tau, salah sendiri lu ngapain ke website ini. Tapi ada tiga kemungkinan kenapa lo
+          bisa ada di website ini. Ketiga kemungkinan itu akan gwej jabarkan di bawah ini dengan
+          ilustrasinya hehehe.
+        </p>
+      </div>
+
+      <div className="grid grid-cols-3 gap-10">
+        {reasons.map((reason, i) => (
+          <div className="space-y-3" key={i}>
+            <div className="aspect-video flex items-center justify-center w-full rounded-xl bg-dark/5">
+              <img src={reason.image} alt={reason.name} />
+            </div>
+            <h3 className="text-[22px] font-semibold">{reason.name}</h3>
+            <p className="opacity-60">{reason.description}</p>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
+
+export default ReasonSection;
