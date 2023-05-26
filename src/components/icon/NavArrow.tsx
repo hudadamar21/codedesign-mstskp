b65eb2fd0a7ecd35557b9dff3198690bf41c4097
hudadamar21 <hudadamar21@gmail.com>
2023-05-26 21:10:13 +0700

    add swiper js for list of alternative

---
+++ src/components/icon/NavArrow.tsx
@@ -0,0 +1,17 @@
+export function NavArrowRight() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" className="w-8 md:w-12 h-8 md:h-12" viewBox="0 0 24 24">
+      <path fill="currentColor" d="M6.23 20.23L8 22l10-10L8 2L6.23 3.77L14.46 12z"></path>
+    </svg>
+  );
+}
+
+export function NavArrowLeft() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" className="w-8 md:w-12 h-8 md:h-12" viewBox="0 0 24 24">
+      <path
+        fill="currentColor"
+        d="M17.51 3.87L15.73 2.1L5.84 12l9.9 9.9l1.77-1.77L9.38 12l8.13-8.13z"></path>
+    </svg>
+  );
+}
--- src/components/sections/AlternativeSection.tsx
+++ src/components/sections/AlternativeSection.tsx
@@ -1,9 +1,15 @@
+import { Navigation } from 'swiper';
+import { Swiper, SwiperSlide } from "swiper/react";
 import { alternatives } from "../../data/alternatives";
 import ArrowRight from "../icon/ArrowRight";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { NavArrowLeft, NavArrowRight } from "../icon/NavArrow";
 
 function AlternativeSection() {
   return (
-    <section className="relative z-10 py-32 text-white mt-64 bg-white">
+    <section id="alternatives" className="relative z-10 py-32 text-white mt-64 bg-white">
       <div className="absolute inset-0 z-[-1] bg-primary w-[98.5%] mx-auto rounded-3xl rotate-0 h-[720px]"></div>
       <div className="absolute inset-0 bg-dark skew-x-1 -skew-y-1 w-[98.5%] mx-auto -rotate-1 rounded-3xl h-[720px]"></div>
       <div className="relative z-10 main-container grid grid-cols-1 md:grid-cols-2 items-center gap-5 md:gap-10 px-10">
@@ -24,17 +30,46 @@ function AlternativeSection() {
           </p>
         </div>
       </div>
-      <div className="relative z-10 main-container bg-white rounded-3xl p-10 md:p-20 grid grid-cols-1 md:grid-cols-2 gap-10 text-black mt-12 md:mt-20 shadow-2xl mx-5">
-        {alternatives.map((alternative, i) => (
-          <div className="space-y-6" key={i}>
-            <img className="rounded-lg w-full" src={alternative.thumbnail} alt={alternative.name} />
-            <p className="text-[22px]">{alternative.description}</p>
-            <a href={alternative.url} className="flex items-center gap-3 font-semibold">
-              Learn More
-              <ArrowRight />
-            </a>
-          </div>
-        ))}
+      <div className="relative z-10 main-container mx-5 md:mx-auto bg-white rounded-3xl p-10 md:p-20 text-black mt-12 md:mt-20 shadow-2xl">
+        <Swiper
+          modules={[Navigation]}
+          spaceBetween={30}
+          slidesPerView={1}
+          navigation={{
+            nextEl: ".alternative-next-slide",
+            prevEl: ".alternative-prev-slide",
+          }}
+          breakpoints={{
+            640: {
+              slidesPerView: 2,
+            },
+          }}
+          className="grid grid-cols-1 md:grid-cols-2 gap-10">
+          {alternatives.map((alternative, i) => (
+            <SwiperSlide key={i}>
+              <div className="space-y-6 ">
+                <img
+                  className="rounded-lg w-full"
+                  src={alternative.thumbnail}
+                  alt={alternative.name}
+                />
+                <p className="text-base md:text-lg lg:text-[22px]">{alternative.description}</p>
+                <a
+                  href={alternative.url}
+                  className="flex items-center gap-3 font-semibold text-sm md:text-base">
+                  Learn More
+                  <ArrowRight />
+                </a>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+        <button className="alternative-next-slide absolute top-[35%] right-0 z-20 text-primary transition duration-200" type="button">
+          <NavArrowRight/>
+        </button>
+        <button className="alternative-prev-slide absolute top-[35%] left-0 z-20 text-primary transition duration-200" type="button">
+          <NavArrowLeft/>
+        </button>
       </div>
     </section>
   );
