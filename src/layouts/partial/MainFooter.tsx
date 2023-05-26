580b13db351cddc93ce80de26acc11acb2f42d32
hudadamar21 <hudadamar21@gmail.com>
2023-05-26 21:18:26 +0700

    padding 💀

--- src/layouts/partial/MainFooter.tsx
+++ src/layouts/partial/MainFooter.tsx
@@ -2,7 +2,7 @@ import { footerMenus } from "../../data/footer-menu";
 
 function MainFooter() {
   return (
-    <footer className="bg-white pb-32 p-10">
+    <footer className="bg-white pb-12 md:pb-32 p-10">
       <div className="main-container grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-10 md:gap-20">
         {footerMenus.map((menu, index) => (
           <div key={index}>
