bfe610d6e8900e4883a9c1a3b977e5cdbd7b8df3
hudadamar21 <hudadamar21@gmail.com>
2023-05-26 21:18:04 +0700

    improve top section

--- src/components/sections/TopSection.tsx
+++ src/components/sections/TopSection.tsx
@@ -4,7 +4,7 @@ function TopSection() {
   return (
     <section
       id="top"
-      className="relative bg-[size:auto_100%] lg:bg-[size:100%_auto] bg-no-repeat pt-24 md:pt-56 pb-32"
+      className="relative bg-[size:auto_100%] lg:bg-[size:100%_auto] bg-no-repeat pt-40 md:pt-56 pb-32"
       style={{ backgroundImage: "url(/images/bg-hero-section.svg)" }}>
       {/* <img
         className="absolute top-0 inset-0 w-full origin-top scale-y-"
@@ -14,12 +14,12 @@ function TopSection() {
 
       <div className="relative main-container z-10 text-center text-white">
         <img
-          className="absolute top-16 left-0 md:left-24 opacity-30 md:opacity-100"
+          className="absolute -top-16 md:top-16 -left-8 md:left-24 opacity-30 md:opacity-100"
           src="/images/dc1.svg"
           alt="decoration 1"
         />
         <img
-          className="absolute top-0 right-0 md:right-24 opacity-30 md:opacity-100"
+          className="absolute top-36 -right-2 md:right-24 opacity-30 md:opacity-100"
           src="/images/dc2.svg"
           alt="decoration 2"
         />
@@ -38,10 +38,10 @@ function TopSection() {
         <div className="font-dm-sans flex items-center justify-center gap-6 md:gap-14 my-20">
           <a
             href="#features"
-            className="px-6 md:px-10 py-4 md:py-6 bg-primary hover:saturate-200 transition duration-300 rounded-full font-bold shadow-2xl shadow-primary/30">
+            className="px-6 md:px-10 py-4 md:py-6 bg-primary text-sm md:text-base hover:saturate-200 transition duration-300 rounded-full font-bold shadow-2xl shadow-primary/30">
             Memulai Sekarang
           </a>
-          <a href="#features" className="flex items-center gap-x-3 font-bold">
+          <a href="#features" className="flex items-center gap-x-3 text-sm md:text-base font-bold">
             <PlayIcon />
             Pelajari Dulu
           </a>
