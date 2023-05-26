5809a9878706eb7db1020940c1ee7bb7f18c424a
hudadamar21 <hudadamar21@gmail.com>
2023-05-26 21:08:33 +0700

    add icons

--- src/components/icon/ArrowRight.tsx
+++ src/components/icon/ArrowRight.tsx
@@ -1,6 +1,6 @@
 function ArrowRight() {
   return (
-    <svg width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg className="w-5 h-5 md:w-6 md:h-6 md:mb-0 -mb-1" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path
         d="M5.5 12H19.5"
         stroke="#0D0E25"
